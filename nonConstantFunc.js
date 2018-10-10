54bb2dee7fc93b5dead25c81c75828e895465045
piggona <2803302887@qq.com>
2018-10-10 14:15:21 +0800

    2018-10-10am

--- nonConstantFunc.js
+++ nonConstantFunc.js
@@ -284,22 +284,34 @@ let abi = [
 
 // 建立一个wallet对象
 let provider = ethers.getDefaultProvider('ropsten');
-let secretKey = "81871A13B79CF60BF8D1D263BAD6B4B6951DC7BCA60ABF09270D2DB2BD6FA60C";
-let wallet = new wallet(secretKey,provider);
+let secretKey = "2EE2182D5AE8FE2B4C11BA7B7FB40930049E2EEF7C5486A7F513F698EBA484DF";
+let wallet = new ethers.Wallet(secretKey,provider);
+
+//获取账户余额
+let balancePromise = wallet.getBalance();
+
+balancePromise.then((balance) => {
+	console.log(balance);
+});
 
 // 合约部署的地址
-let contractAddress = "0x570632c529416764f5e622c90f89c36d5963d702";
+let contractAddress = "0xd1b57e366d432f7080d73b4c6c0e31abcab4da7a";
 
 // 使用钱包对象建立的Contract对象具有读写权限
-let contract = new ethers.Contract(contractAddress, abi, wallet);
+let contract = new ethers.Contract(contractAddress, abi, provider);
+let contractWithSigner =contract.connect(wallet);
 
 // 进行对nonConstantFunc的操作
-let tx = await contract.repayment_suc();
+// let tx = await contractWithSigner.repayment_suc();
 
-console.log(tx.hash);
+// console.log(tx.hash);
 
-await tx.wait();
+// await tx.wait();
 
-let currentBalance = await contract.getBalance();
+let currentBalance = contract.get_balance();
 
-console.log(currentBalance);
+currentBalance.then((balance) => {
+	console.log(balance);
+}).catch((err) => {
+	console.log(err);
+});
